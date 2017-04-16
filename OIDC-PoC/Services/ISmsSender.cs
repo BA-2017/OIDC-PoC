2111b26f168180dafbf6839776f7c185b54f79d6
Beni Warmers <benjamin.warmers@voicepoint.ch>
2017-04-16 15:39:09 +0200

    ASP.NET core mvc template with individual user accounts

---
+++ OIDC-PoC/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OIDC-PoC.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
